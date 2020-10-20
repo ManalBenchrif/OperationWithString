f9932811380afdcf4876b20b4276fe0d2afdbb5d
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-20 02:01:04 +0100

    Add files via upload

---
+++ OperationWithString.py
@@ -0,0 +1,10 @@
+# Mixing operators between numbers and strings is not supported
+name1="Hello"
+name2="i'm manal"
+name3="^_^"
+concat=name1+" "+name2+" "+name3 # Python supports concatenating strings using the addition operator:
+print(concat)
+#Python also supports multiplying strings to form a string with a repeating sequence:
+name="manal"
+new_name=name*5
+print(new_name)
